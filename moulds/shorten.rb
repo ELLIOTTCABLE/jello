a6abcaf310058a9a2222a184a77aeb5896596b3c
elliottcable <git@elliottcable.name>
2008-10-04 16:43:09 -0800

    Added uersname and password support to the tr.im shortener example

--- moulds/shorten.rb
+++ moulds/shorten.rb
@@ -8,7 +8,15 @@ Jello::Mould.new do |paste, board|
     unless paste =~ %r{^http://tr.im}
       uri.gsub! /#/, '%23' # Fix anchors
       
-      shortener = 'http://tr.im/api/trim_url.json?url=' + uri
+      # Feel free to copy this Mould to your ~/.jello directory and hardcode
+      # in your username and password, if you don't feel like having your
+      # username and password in your shell history.
+      params = {}
+      params[:username] = ENV['TRIM_USERNAME'] if ENV['TRIM_USERNAME']
+      params[:password] = ENV['TRIM_PASSWORD'] if ENV['TRIM_PASSWORD']
+      params[:url] = uri
+      
+      shortener = 'http://tr.im/api/trim_url.json?' + params.to_a.map {|a| a.join '=' }.join '&'
       
       reply = open(shortener).read
       short = JSON.parse reply
